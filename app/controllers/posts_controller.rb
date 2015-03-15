1653176d178e80fbdf7bc2fa05346053c6e2b201
Michael Angelo <yamikamisama@gmail.com>
2015-03-15 19:52:58 -0400

    connecting create controller to view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,8 +16,12 @@ class PostsController < ApplicationController
 	end
 
 	def create
-		post = Post.create(post_params)
-		redirect_to posts_path
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to post_path
+		else
+			redirect_to new_post_path
+		end
 	end
 
 	def edit
