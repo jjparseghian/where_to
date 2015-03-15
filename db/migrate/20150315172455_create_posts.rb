2be2bd55d00cbda353c64ed773df304b281cd880
James Parseghian <jjparseghian@gmail.com>
2015-03-15 13:40:14 -0400

    Add posts migration

---
+++ db/migrate/20150315172455_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title null: false
+      t.string :location null: false
+      t.text :description null: false
+      t.references :user
+
+      t.timestamps null: false
+    end
+  end
+end
