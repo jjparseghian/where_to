25f015e37a29c6de630e6abc75baf93d9e1f91d4
Michael Angelo <yamikamisama@gmail.com>
2015-03-15 15:24:27 -0400

    add seed file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+	User.create(username: Faker::Name.name, email: Faker::Internet.email, password: '1234', )
+	Post.create(title: Faker::Address.city, location: Faker::Address.country, description: Faker::Lorem.paragraph)
+end
