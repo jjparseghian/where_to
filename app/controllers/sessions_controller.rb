e33fe2f5be2df4f43f5bba89b9896da06c7d362a
James Parseghian <jjparseghian@gmail.com>
2015-03-18 00:55:30 -0400

    Redirect to welcome page after login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
 
   def create
-    @user = User.find_by_username(params[:username])
+    @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect_to root_url, notice: "Logged in"
+      redirect_to '/', notice: "Logged in"
     else
       flash.now.alert = "Username or password is invalid"
       render "new"
@@ -13,6 +13,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, notice: "Logged out"
+    redirect_to '/', notice: "Logged out"
   end
 end
