ebdfe2864d3b4ad4172442b8ee5d734ef3f1ce32
James Parseghian <jjparseghian@gmail.com>
2015-03-18 22:17:51 -0400

    Add logic to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
-    def new
+
+  def new
+    @user = User.new
   end
 
   def create
-  end
-
-  def show
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/', notice: "You have successfully signed up!"
+    else
+      render 'new'
+    end
   end
 
   private
 
   def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
+
 end
